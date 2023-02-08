1be2fc91c01f227a4d83e4dc01c671995bd70fb5
Alex Hughes <alxhghs@gmail.com>
2023-02-08 06:58:59 -0800

    add react native messaging

--- App.js
+++ App.js
@@ -2,7 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { initialize } from "@flashcoffee/react-native-zendesk-messaging";
+
 export default function App() {
+  console.log({initialize})
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
